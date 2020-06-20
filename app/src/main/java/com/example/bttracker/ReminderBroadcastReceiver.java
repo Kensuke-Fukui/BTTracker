17587582df112d14dcdd586b161220fe7d85a843
Kensuke-Fukui <2020m076@kuas.ac.jp>
2020-06-20 13:33:40 +0900

    Homework 5.3: extended ReminderBroadcastReceiver to start LogActivity directly from notification"

--- app/src/main/java/com/example/bttracker/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/bttracker/ReminderBroadcastReceiver.java
@@ -1,5 +1,6 @@
 package com.example.bttracker;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,12 +13,15 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
+        Intent notificationIntent = new Intent(context, LogActivity.class);
+        PendingIntent contentInent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
         // an Intent broadcast.
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "BT_Tracker_Channel")//this second argument has to match the Channel ID defined in MainActivity.java
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setContentTitle("Notification from BT Tracker")
                 .setContentText("Please log your body temperature now")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(contentInent);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify(200, builder.build());
     }
